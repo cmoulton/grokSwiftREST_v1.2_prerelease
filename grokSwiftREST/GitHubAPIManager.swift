8324dc8c49a0106bc8d0f5486a0c78f911c2d9bd
Christina Moulton <christina@teakmobile.com>
2016-04-17 15:11:45 -0300

    Print headers to verify that oauth token is being sent in Alamofire request

--- grokSwiftREST/GitHubAPIManager.swift
+++ grokSwiftREST/GitHubAPIManager.swift
@@ -151,7 +151,7 @@ class GitHubAPIManager {
   
   // MARK: - OAuth 2.0
   func printMyStarredGistsWithOAuth2() -> Void {
-    Alamofire.request(GistRouter.GetMyStarred())
+    let alamofireRequest = Alamofire.request(GistRouter.GetMyStarred())
       .responseString { response in
         guard let receivedString = response.result.value else {
           print(response.result.error!)
@@ -160,6 +160,7 @@ class GitHubAPIManager {
         }
         print(receivedString)
     }
+    debugPrint(alamofireRequest)
   }
   
   // MARK: API Calls
