c8552d1151d3c0d82beb19a8769254ae60d53a5e
Michael Langford <michael.langford@rowdylabs.com>
2016-04-29 12:43:06 -0400

    using data struture to remove several conditionals

--- grokSwiftREST/MasterViewController.swift
+++ grokSwiftREST/MasterViewController.swift
@@ -72,18 +72,12 @@ SFSafariViewControllerDelegate {
     
     self.isLoading = false
     if result.error?.domain != NSURLErrorDomain { return }
-    
+
+
     if result.error?.code == NSURLErrorUserAuthenticationRequired {
         self.showOAuthLoginView()
     } else if result.error?.code == NSURLErrorNotConnectedToInternet {
-        let path:Path
-        if self.gistSegmentedControl.selectedSegmentIndex == 0 {
-            path = .Public
-        } else if self.gistSegmentedControl.selectedSegmentIndex == 1 {
-            path = .Starred
-        } else {
-            path = .MyGists
-        }
+        let path:Path = [.Public, .Starred, .MyGists][self.gistSegmentedControl.selectedSegmentIndex]
         
         if let archived:[Gist] = PersistenceManager.loadArray(path) {
             self.gists = archived
